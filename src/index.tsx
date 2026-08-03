62cf4e58d0d9db8985598ebc194b52a3c0970933
quangannhat <quangannhat.dev@gmail.com>
2026-08-04 06:22:03 +0700

    agents.md

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,11 @@
 import { createCliRenderer } from "@opentui/core";
 import { createRoot } from "@opentui/react";
 import { App } from "@app/App";
+import { AppKeymapProvider } from "@app/components/keymap-provider";
 
 const renderer = await createCliRenderer();
-createRoot(renderer).render(<App />);
+createRoot(renderer).render(
+  <AppKeymapProvider>
+    <App />
+  </AppKeymapProvider>,
+);
